2560630ef28d5f8380321809101bd6a450afa899
szymon <szymonkolo555@gmail.com>
2023-11-09 00:49:09 +0100

    Lekka popraweczka

--- 20.10.2023/index.js
+++ 20.10.2023/index.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const homepageRouter = require('./routes/homepage')
 const kontaktRouter = require('./routes/kontakt')
 const apiRouter = require('./routes/api')
-const apiStudentsRouter = require('./routes/api_students')
-const apiSubjectsRouter = require('./routes/api_subjects')
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static('./public/css', { 'extensions': ['css'] }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 
+app.use('/api', apiRouter)
 app.use('/', homepageRouter)
-app.get('/kontakt', kontaktRouter)
-app.post('/kontakt', kontaktRouter)
-app.get('/api', apiRouter)
-app.get('/api/students', apiStudentsRouter)
-app.get('/api/students/:id', apiStudentsRouter)
-app.get('/api/subjects', apiSubjectsRouter)
-app.get('/api/subjects/:id', apiSubjectsRouter)
+app.use('/kontakt', kontaktRouter)
 
 app.listen(3000, '127.0.0.1',()=>
 {
--- 20.10.2023/routes/kontakt.js
+++ 20.10.2023/routes/kontakt.js
@@ -2,11 +2,11 @@ const {Router} = require('express')
 const router = Router()
 const path = require('path')
 
-router.get('/kontakt', (req, res)=>
+router.get('/', (req, res)=>
 {
     res.sendFile(path.join(__dirname, '../public/html/kontakt.html'))
 })
-router.post('/kontakt', (req, res)=>
+router.post('/', (req, res)=>
 {
     console.log(req.body)
     res.redirect('/')
