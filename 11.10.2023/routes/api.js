cb98c6763f81cde1e1afc93b7cc3682bbc003dad
SzKolodziej <szymonkolo555@gmail.com>
2023-11-16 22:44:47 +0100

    mongoł db

---
+++ 11.10.2023/routes/api.js
@@ -0,0 +1,30 @@
+const {Router} = require('express')
+const router = Router()
+const path = require('path')
+const fs = require("fs/promises")
+const {MongoClient, ServerApiVersion} = require('mongodb')
+const password = "PknGQXNmsA2SRr2b"
+const url = `mongodb+srv://szymonkolo555:${password}@zsk.hdziy8p.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(url, {
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+const getMessages = async ()=>{
+    await client.connect()
+    const database = client.db('Project')
+    const collection = database.collection('ZSK')
+
+    const message = await collection.find().toArray()
+    return message
+}
+
+router.get('/', async (req, res)=>{
+    res.json(await getMessages())
+})
+
+module.exports = router
---
+++ 11.10.2023/routes/kontakt.js
@@ -0,0 +1,46 @@
+const { Router } = require('express')
+const router = Router()
+const path = require('path')
+const {MongoClient, ServerApiVersion} = require('mongodb')
+const password = "PknGQXNmsA2SRr2b"
+const url = `mongodb+srv://szymonkolo555:${password}@zsk.hdziy8p.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(url, {
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+const addFormData = async(formData) =>{
+    await client.connect()
+    const database = client.db('Project')
+    const collection = database.collection('ZSK')
+
+    await collection.insertOne(formData)
+}
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/html/kontakt.html'))
+})
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+
+    const {imie, email, select, textarea} = req.body
+    const formData = {}
+    if(imie){
+        formData.imie = imie
+    }
+
+    formData.email = email
+    formData.select = select
+    formData.textarea = textarea
+
+    await addFormData(formData)
+
+    res.redirect('/')
+})
+
+module.exports = router;
